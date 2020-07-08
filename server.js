8557e4639f445766579bede482f78dd98ebfa4a2
h4wk590 <aidanbrownj@gmail.com>
2020-07-08 10:55:11 -0700

    Test pipeline CI

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello BC DevExchange! \n");
+  res.send("Hello BC DevExchange! Test Test Test \n");
 });
 
 app.listen(PORT, HOST, () => {
